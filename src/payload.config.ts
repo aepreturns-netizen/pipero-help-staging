74c6fceb55825d1bf2ae9b24d2dd8d4dfc0f5814
aepreturns-netizen <aep.returns@gmail.com>
2026-09-15 09:36:27 +0700

    Fix rich text table block type

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -85,7 +85,7 @@ export default buildConfig({
     features: ({ defaultFeatures }) => [
       ...defaultFeatures,
       BlocksFeature({
-        blocks: ['richTextTable'],
+        blocks: [RichTextTableBlock],
       }),
     ],
   }),
